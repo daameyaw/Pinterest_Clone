7ab3976d8493cad97cb91d44367039c603c79e69
Asante.dev <davidameyaw607@gmail.com>
2024-07-04 19:39:28 -0800

    Adding for data

--- assets/data/pins.js
+++ assets/data/pins.js
@@ -4,12 +4,24 @@ export default [
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg",
     title: "notJust Dev Hoodie",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "1",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
     title: "Programmer working on laptop computer in office studio",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "2",
@@ -17,36 +29,72 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/2.jpeg",
     title:
       "computer setup | computer setup idea | black wallpaper #computersetupideas",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "3",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/3.jpeg",
     title: "",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "4",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/4.jpeg",
     title: "White Tesla ",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "5",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/5.jpeg",
     title: "Hustle harder",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "6",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/6.jpeg",
     title: "Studio",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "7",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/7.jpeg",
     title: "The Flying tortoise",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "8",
@@ -54,11 +102,23 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/8.jpeg",
     title:
       "Harley Davidson Sportster Iron 883 Custom ~ Rider & Helmet - ModifiedX",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
   {
     id: "9",
     image:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/9.jpeg",
     title: "Tesla Cybertruck",
+    owner: "Phuong",
+    ownerImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+    followers: "874",
+    likes: "3.5k",
+    comments: "93",
   },
 ];
