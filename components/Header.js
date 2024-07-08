bb3b0513e391cadba83835719b9a6b38f0a68b8d
Asante.dev <davidameyaw607@gmail.com>
2024-07-07 20:00:38 -0800

    A reusable header component

---
+++ components/Header.js
@@ -0,0 +1,37 @@
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header({ title }) {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.header}>
+      <AntDesign
+        onPress={() => navigation.goBack()}
+        name="arrowleft"
+        size={24}
+        color="black"
+      />
+      <Text style={styles.headerTitle}>{title}</Text>
+      <View style={styles.headerRight} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  headerTitle: {
+    flex: 1,
+    textAlign: "center",
+    fontSize: 21,
+    fontWeight: "bold",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+  },
+  headerRight: {
+    width: 24,
+  },
+});
