3a6a9b12cc03038676a68502f7d9561e52dd3ad1
Asante.dev <davidameyaw607@gmail.com>
2024-07-08 21:17:23 -0800

    Pins

---
+++ assets/data/CameraPins.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    id: "0",
+    image:
+      "https://images.unsplash.com/photo-1521499672281-c8be7d37d451?w=500&h=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG9ncmFwaHklMjBjYW1lcmF8ZW58MHx8MHx8fDI%3D",
+    title: "",
+    owner: "Maurice",
+    ownerImage:
+      "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "92k",
+    likes: "16k",
+    comments: "28",
+  },
+  {
+    id: "1",
+    image:
+      "https://images.unsplash.com/photo-1617459840892-6d97e6359eae?w=800&h=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG9ncmFwaHklMjBjYW1lcmF8ZW58MHx8MHx8fDI%3D",
+    title: "",
+    owner: "Callie",
+    ownerImage:
+      "https://images.unsplash.com/photo-1464863979621-258859e62245?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "7K",
+    likes: "22K",
+    comments: "16",
+  },
+  {
+    id: "2",
+    image:
+      "https://images.unsplash.com/photo-1667590716915-30654eddece2?w=700&h=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGhvdG9ncmFwaHklMjBjYW1lcmF8ZW58MHx8MHx8fDI%3D",
+    title: "",
+    owner: "Nora",
+    ownerImage:
+      "https://images.unsplash.com/photo-1543096222-72de739f7917?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "33K",
+    likes: "100K",
+    comments: "9",
+  },
+  {
+    id: "3",
+    image:
+      "https://images.unsplash.com/photo-1704886218193-f1cd154ac910?w=3000&h=4300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG9ncmFwaHklMjBjYW1lcmF8ZW58MHx8MHx8fDI%3D",
+    title: "",
+    owner: "Amanda",
+    ownerImage:
+      "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?w=1000&h=2500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "19k",
+    likes: "32k",
+    comments: "72",
+  },
+  {
+    id: "4",
+    image:
+      "https://images.unsplash.com/photo-1635443182868-10de46b732d3?w=700&h=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBob3RvZ3JhcGh5JTIwY2FtZXJhfGVufDB8fDB8fHwy",
+    title: "",
+    owner: "Ola",
+    ownerImage:
+      "https://images.unsplash.com/photo-1712847331925-bf0e3fd2b7ae?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MTV8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "60k",
+    likes: "78k",
+    comments: "2",
+  },
+  {
+    id: "5",
+    image:
+      "https://images.unsplash.com/photo-1697797390879-016c2b7148fb?w=1500&h=2000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBob3RvZ3JhcGh5JTIwY2FtZXJhfGVufDB8fDB8fHwy",
+    title: "",
+    owner: "Tony",
+    ownerImage:
+      "https://images.unsplash.com/photo-1521146764736-56c929d59c83?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "22k",
+    likes: "53k",
+    comments: "79",
+  },
+  {
+    id: "6",
+    image:
+      "https://images.unsplash.com/photo-1652562809397-447a2a5fade2?w=1400&h=2600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBob3RvZ3JhcGh5JTIwY2FtZXJhfGVufDB8fDB8fHwy",
+    title: "",
+    owner: "Adam",
+    ownerImage:
+      "https://images.unsplash.com/photo-1509460913899-515f1df34fea?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "89k",
+    likes: "85k",
+    comments: "80",
+  },
+  {
+    id: "7",
+    image:
+      "https://images.unsplash.com/photo-1718977204637-88ea8253ac9c?w=500&h=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGhvdG9ncmFwaHklMjBjYW1lcmF8ZW58MHx8MHx8fDI%3D",
+    title: "",
+    owner: "Charlotte",
+    ownerImage:
+      "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "2k",
+    likes: "21k",
+    comments: "8",
+  },
+  {
+    id: "8",
+    image:
+      "https://images.unsplash.com/photo-1483034806857-f6e6310e7506?q=80&w=3050&h=4400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "",
+    owner: "Rachel",
+    ownerImage:
+      "https://images.unsplash.com/photo-1506863530036-1efeddceb993?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "9k",
+    likes: "97k",
+    comments: "81",
+  },
+  {
+    id: "9",
+    image:
+      "https://images.unsplash.com/photo-1483034806857-f6e6310e7506?q=80&w=2050&h=4600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "",
+    owner: "Rachel",
+    ownerImage:
+      "https://images.unsplash.com/photo-1506863530036-1efeddceb993?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZhY2V8ZW58MHx8MHx8fDI%3D",
+    followers: "9k",
+    likes: "97k",
+    comments: "81",
+  },
+];
