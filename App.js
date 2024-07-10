0e8ce36dab6af4eb6b4751f74f30ad5878724e55
Asante.dev <davidameyaw607@gmail.com>
2024-07-09 22:10:26 -0800

    cds

--- App.js
+++ App.js
@@ -5,13 +5,33 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigator from "./Stacks/TabNavigator";
 import StackNavigator from "./Stacks/StackNavigator";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Toast from "react-native-toast-message";
 
 function App() {
   return (
     <>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+        <Toast
+          position="bottom"
+          style={{ borderLeftColor: "pink" }}
+          contentContainerStyle={{ padding: 15 }}
+          text1Style={{
+            fontSize: 20,
+            fontWeight: "400",
+            paddingVertical: 5,
+          }}
+          text2Style={{
+            fontSize: 16,
+            paddingBottom: 10,
+          }}
+          bottomOffset={90}
+        />
+      </Provider>
     </>
   );
 }
