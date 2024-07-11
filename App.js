ef2063c12b95b8fb174fb156a2fd529cae246495
Asante.dev <davidameyaw607@gmail.com>
2024-07-10 22:22:14 -0800

    Update

--- App.js
+++ App.js
@@ -8,8 +8,23 @@ import StackNavigator from "./Stacks/StackNavigator";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import Toast from "react-native-toast-message";
+import { onAuthStateChanged } from "firebase/auth";
+import { FIREBASE_AUTH } from "./FirebaseConfig";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import StartScreen from "./screens/StartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUp from "./screens/SignUp";
+
+const Stack = createNativeStackNavigator();
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
+  }, []);
   return (
     <>
       <Provider store={store}>
