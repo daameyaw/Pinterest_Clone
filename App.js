b223dcd64998b894ffd1e1a65be93a515c671e25
Asante.dev <davidameyaw607@gmail.com>
2024-07-17 09:53:30 -0800

    Created custom toast layout

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import TabNavigator from "./Stacks/TabNavigator";
 import StackNavigator from "./Stacks/StackNavigator";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import Toast from "react-native-toast-message";
+import Toast, { BaseToast } from "react-native-toast-message";
 // import { onAuthStateChanged } from "firebase/auth";
 // import { FIREBASE_AUTH } from "./FirebaseConfig";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -25,6 +25,27 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+const toastConfig = {
+  /*
+    Overwrite 'success' type,
+    by modifying the existing `BaseToast` component
+  */
+  success: (props) => (
+    <BaseToast
+      {...props}
+      position="top"
+      style={{ borderLeftColor: "black" }}
+      contentContainerStyle={{ paddingHorizontal: 45, borderRadius: 40 }}
+      text1Style={{
+        fontSize: 20,
+        fontWeight: "400",
+        paddingVertical: 5,
+      }}
+      topOffset={140}
+    />
+  ),
+};
 function App() {
   const [user, setUser] = React.useState(null);
 
@@ -40,21 +61,7 @@ function App() {
           <NavigationContainer>
             <StackNavigator />
           </NavigationContainer>
-          <Toast
-            position="bottom"
-            style={{ borderLeftColor: "pink" }}
-            contentContainerStyle={{ padding: 15 }}
-            text1Style={{
-              fontSize: 20,
-              fontWeight: "400",
-              paddingVertical: 5,
-            }}
-            text2Style={{
-              fontSize: 16,
-              paddingBottom: 10,
-            }}
-            bottomOffset={90}
-          />
+          <Toast config={toastConfig} />
         </Provider>
       </QueryClientProvider>
     </>
@@ -62,3 +69,26 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <Toast
+position="top"
+style={{
+  borderLeftColor: "black", // Set the border left color to black
+  borderLeftWidth: 5, // Ensure there is a border width, adjust as needed
+  backgroundColor: "white", // Optional: set a background color for better visibility
+  borderRadius: 100, // Optional: add border radius for styling
+  margin: 10,
+}} // Optional: add margin for spacing            contentContainerStyle={{ padding: 15 }}
+text1Style={{
+  fontSize: 20,
+  fontWeight: "400",
+  paddingVertical: 5,
+}}
+text2Style={{
+  fontSize: 16,
+  paddingBottom: 10,
+}}
+topOffset={90}
+/> */
+}
