e901118464a64e7cb2dc1a3b843079139629e973
Asante.dev <davidameyaw607@gmail.com>
2024-07-18 22:15:10 -0800

    change

--- screens/CountiesScreen.js
+++ screens/CountiesScreen.js
@@ -89,8 +89,8 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+    // borderBottomWidth: 1,
+    // borderBottomColor: "#ccc",
   },
   text: {
     fontSize: 18,
